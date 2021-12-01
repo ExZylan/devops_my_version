f5175b73ca7d2b44bc98409e457e53041073bda5
ExZylan <35953678+ExZylan@users.noreply.github.com>
2021-12-01 20:01:38 +0100

    Solve Destroy Cluster problems and add workflow_dispatch in Deploy Services to cluster workflow (#20)
    * Add docker-compose-swarm.yaml and update test_actions.yml
    
    * Add ansible to deploy swarm cluster in EC2
    
    * Add ansible to deploy swarm cluster in EC2
    
    * prepare Github actions to deploy the cluster automatically and clean files
    
    * change permissions of deploy.sh file
    
    * Solve Destroy Cluster problems
    
    * Test add workflow_dispatch in Deploy Services to cluster workflow
    
    * solve permissons .sh
    
    Co-authored-by: zylan <j.d.albiolgmail.com>

--- .github/scripts/destroy_cluster.sh
+++ .github/scripts/destroy_cluster.sh
@@ -1,16 +1,31 @@
-name: Destroy the cluster
-on:
-  workflow_dispatch:
-jobs:
-  deploy:
-    runs-on: ubuntu-20.04
-    steps:
-      - name: Check out repository code
-        uses: actions/checkout@v2
+#!/bin/sh
 
-      - name: Destroy the cluster
-        run: .github/scripts/destroy_cluster.sh
-        env:
-          VAULT_PASS: ${{ secrets.VAULT_PASS }}
-          AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+# Terraform install
+wget -q https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip
+unzip -o terraform_1.0.11_linux_amd64.zip -d ~
+
+# Use Terraform to collect the cluster ips
+cd terraform/pipeline
+~/terraform init
+~/terraform apply -auto-approve
+~/terraform output | grep '"' | awk -F '"' 'BEGIN {i=1; print "[cluster]"} {print "node0" i++ " ansible_host="$2}' > ../../terraform/ansible/hosts.ini
+cd ../..
+
+# Ansible configuration
+mkdir ~/.ssh
+
+echo $VAULT_PASS > ~/vault_passowrd_file.txt
+
+ansible-vault decrypt \
+  --vault-password-file=~/vault_passowrd_file.txt terraform/ansible/id_rsa_devops_encrypted \
+  --output=~/.ssh/id_rsa
+
+rm ~/vault_passowrd_file.txt
+
+# Use Ansible to configure the cluster
+ansible-playbook -u ubuntu -i terraform/ansible/hosts.ini terraform/ansible/swarm/destroy_swarm.yml
+
+# Use Terraform to destroy the cluster
+cd terraform/terraform_aws
+~/terraform init
+~/terraform destroy -auto-approve
